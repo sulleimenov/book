b404ded2888a1f204e45dcb43a958fefc994cb32
Sulleimenov <sulleimenov@yandex.ru>
2025-02-26 02:09:43 +0500

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+// import '~/app/vendor/mmenu/dist/mmenu.js'
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+	document.querySelectorAll('textarea').forEach(el => {
+    el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+    el.classList.add('auto');
+    el.addEventListener('input', e => {
+        el.style.height = 'auto';
+        el.style.height = (el.scrollHeight) + 'px';
+    });
+});
+
+});
