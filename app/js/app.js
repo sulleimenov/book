2a7693f1c9d783e87b438d5f998a5b1d6a17a720
Sulleimenov <sulleimenov@yandex.ru>
2025-02-26 13:43:40 +0500

    fix copy

--- app/js/app.js
+++ app/js/app.js
@@ -4,12 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 	document.querySelectorAll('textarea').forEach(el => {
-    el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
-    el.classList.add('auto');
-    el.addEventListener('input', e => {
-        el.style.height = 'auto';
-        el.style.height = (el.scrollHeight) + 'px';
-    });
-});
+		el.style.height = el.setAttribute('style', 'height: ' + el.scrollHeight + 'px');
+		el.classList.add('auto');
+		el.addEventListener('input', e => {
+				el.style.height = 'auto';
+				el.style.height = (el.scrollHeight) + 'px';
+		});
+	});
+
+	document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+	document.addEventListener("keydown", (event) => {
+			if (
+					(event.ctrlKey && ["c", "x", "v", "s", "u", "p"].includes(event.key.toLowerCase())) ||
+					event.key === "F12"
+			) {
+					event.preventDefault();
+			}
+	});
 
 });
